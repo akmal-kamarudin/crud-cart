bdd6d33c60a63c69b19c742207c3902b06cddf84
akmal-kamarudin <akmal.kamarudin19@gmail.com>
2023-07-25 08:51:43 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+// const { auth } = require("express-openid-connect");
+
+const express = require("express");
+const { auth, requiresAuth } = require("express-openid-connect");
+const app = express();
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: "e206b769598200650da69a3944b4da2e23ed7420c8d7c906df1f3c7849c1081f", //random secret
+  baseURL: "http://localhost:3000",
+  clientID: "GVMPSsrj0aTeBti4rexbxbDBqnFyKj5T",
+  issuerBaseURL: "https://akmal-busy.us.auth0.com",
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get("/", (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? "Logged in" : "Logged out");
+});
+
+// The /profile route will show the user profile as JSON
+app.get("/profile", requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user, null, 2));
+});
+
+app.listen(3000, function () {
+  console.log("Listening on http://localhost:3000");
+});
+
+// http://localhost:3000/Login , /Logout , /profile
